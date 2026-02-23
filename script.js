42b7445f46829e5d4eeff91464def75e9239c50c
Svartakatten <rikexhx@gmail.com>
2026-02-23 23:12:53 +0100

    fix: removed function simulateHeavyWork, changed variables to const, changed listening to click -> submit

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-(function simulateHeavyWork() {
-    var start = Date.now();
-    while (Date.now() - start < 200) {}
-})();
-
 document.addEventListener("DOMContentLoaded", function () {
-    var items = document.querySelectorAll(".add-to-cart");
+    const items = document.querySelectorAll(".add-to-cart");
     items.forEach(function (item) {
         item.addEventListener("click", function () {
             alert("Item added to cart!");
         });
     });
 
-    var newsletterButton = document.querySelector(".newsletter .primary-button");
+    const newsletterButton = document.querySelector(".newsletter .primary-button");
     if (newsletterButton) {
-        newsletterButton.addEventListener("click", function (e) {
+        newsletterButton.addEventListener("submit", function (e) {
             e.preventDefault();
             alert("Thank you for subscribing!");
         });
